e1161b25729e2d7ebfb3ea25906c6ff1ad17ae4c
Leonardo Ortiz Ramirez <lortizr@uniremingtonmanizales.edu.co>
2020-10-03 00:26:11 -0500

    Se creo la Clase Abstracta Poligono.

---
+++ EjemploAreasPoligonos/src/ejemploareaspoligonos/Poligono.java
@@ -0,0 +1,23 @@
+package ejemploareaspoligonos;
+
+public abstract class Poligono {
+    protected int numeroLados;
+    
+    public Poligono(int numeroLados){
+        this.numeroLados = numeroLados;      
+    }
+    public int getNumeroLados() {
+        return numeroLados;
+    }
+    public void setNumeroLados(int numeroLados) {
+        this.numeroLados = numeroLados;
+    }
+
+    @Override
+    public String toString() {
+        return "Numero de lados = " + numeroLados;
+    }
+    
+    public abstract double area();
+    
+}
